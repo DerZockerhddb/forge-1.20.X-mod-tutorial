fd2a0aed0320496cf0df8b4807d405dacbe7ae32
DerZockerhddb <domenik2009@freenet.de>
2024-09-03 19:11:02 +0200

    Initial commit

--- src/main/java/net/DerZockerhddb/Loot_boxmod/block/ModBlocks.java
+++ src/main/java/net/DerZockerhddb/Loot_boxmod/block/ModBlocks.java
@@ -6,7 +6,6 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -20,7 +19,7 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, Loot_boxMod.MOD_ID);
 
 public static final RegistryObject<Block> VENDINGMACHINE_BLOCK = registerBlock("vendingmachine_block",
-    () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+    () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK)));
 
     private static <T extends Block> RegistryObject<T> registerBlock (String name, Supplier<T> block ) {
 RegistryObject<T> toReturn = BLOCKS.register(name, block);
